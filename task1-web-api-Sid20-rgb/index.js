0212705de9e19928c961c1706706384b574368f1
Siddhartha Shakya <75927865+Sid20-rgb@users.noreply.github.com>
2024-02-01 22:06:38 +0545

    Set http

--- task1-web-api-Sid20-rgb/index.js
+++ task1-web-api-Sid20-rgb/index.js
@@ -1,6 +1,29 @@
 const app = require('./app')
-const port = process.env.PORT 
+const port = process.env.PORT
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`)
 });
+
+//..................
+
+// const app = require("./app");
+// // const port = process.env.PORT
+// const https = require("https");
+// const fs = require("fs");
+
+// const port = process.env.PORT;
+// https
+//   .createServer(
+//     {
+//       cert: fs.readFileSync("./localhost.crt"),
+//       key: fs.readFileSync("./localhost.key"),
+//     },
+//     app
+//   )
+//   .listen(port);
+//.............................
+
+// app.listen(port, () => {
+//   console.log(`Server is running on port ${port}`)
+// });
